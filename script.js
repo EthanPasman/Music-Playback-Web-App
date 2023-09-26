8ea2aae63d74c471d8d1a96c91b5878ea55ea13c
EthanPasman <epasman123@gmail.com>
2023-09-26 18:55:23 -0400

    Fixed some issues

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
 var supportedExtensions = ["mp3", "wav", "ogg"];
 var fileData = ["", "", "", ""]; //URL, Title, Artist, Album
+var genres = [];
+var tags = [];
 
 function addMetadata() {
     fileData[1] = document.getElementById("title").value;
     fileData[2] = document.getElementById("artist").value;
     fileData[3] = document.getElementById("album").value;
     document.getElementById("displayFileData").innerHTML = fileData[2] + " - " + fileData[1];
+    clearForm();
     document.getElementById("metadataForm").style.display = "none";
+}
+
+function clearForm() {
+    document.getElementById("genreList").textContent = "";
+    document.getElementById("tagList").textContent = "";
+    document.getElementById("rating").textContent = "";
+    genres = [];
+    tags = [];
+    var formElements = document.getElementById("metadataForm").getElementsByTagName("input");
+    for (i = 0; i < formElements.length; i++) {
+        var fieldType = formElements[i].type;
+        switch (fieldType) {
+            case "text":
+            case "number":
+                formElements[i].value = "";
+                break;
+            case "radio":
+                formElements[i].checked = false;
+                break;
+        }
+    }
+    contractForm();
 }
 
 function extendForm() {
     extend.style.display = "none";
-    contract.style.display = "inherit";
-    expandedForm.style.display = "inherit";
+    contract.style.display = "initial";
+    expandedForm.style.display = "initial";
 }
 
 function contractForm() {
-    extend.style.display = "inherit";
+    extend.style.display = "initial";
     contract.style.display = "none";
     expandedForm.style.display = "none";
+}
+
+function addGenre() {
+    genres.push(document.getElementById("genre").value);
+    document.getElementById("genreList").textContent = genres.toString();
+    document.getElementById("genre").value = "";
+}
+
+function addTag() {
+    tags.push(document.getElementById("tags").value);
+    document.getElementById("tagList").textContent = tags.toString();
+    document.getElementById("tags").value = "";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -30,21 +67,12 @@ document.addEventListener("DOMContentLoaded", function () {
     var extend = document.getElementById("extend");
     var contract = document.getElementById("contract");
     var expandedForm = document.getElementById("expandedForm");
-    var formReset = document.getElementById("formReset");
     var rating = document.getElementById("rating");
-    var genreList = document.getElementById("genreList");
-    var tagList = document.getElementById("tagList");
-    var genres = []
-    var tags = []
 
     unsupportedFileType.style.display = "none";
     metadataForm.style.display = "none";
     contract.style.display = "none";
     expandedForm.style.display = "none";
-
-    formReset.addEventListener("click", function () {
-        rating.textContent = "";
-    });
 
     var radioButtons = document.querySelectorAll('input[type="radio"]');
     radioButtons.forEach((radioButton) => {
@@ -58,18 +86,6 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    function addGenre() {
-        genres.push(document.getElementById(genre).value);
-        genreList.value = genres.toString();
-        document.getElementById(genre).value = "";
-    }
-
-    function addTag() {
-        tags.push(document.getElementById(tags).value);
-        tagList.value = tags.toString();
-        document.getElementById(tags).value = "";
-    }
-
     function fileListener() {
         unsupportedFileType.style.display = "none";
         var selectedFile = fileInput.files[0];
@@ -81,9 +97,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 audioPlayback.src = objURL;
                 audio.load();
                 fileData[0] = objURL;
-                metadataForm.style.display = "inherit";
+                metadataForm.style.display = "initial";
             } else {
-                unsupportedFileType.style.display = "inherit";
+                unsupportedFileType.style.display = "initial";
             }
         }
     }
