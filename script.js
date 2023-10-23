8536ecace488b6c2eacb4ff15a3188594a014cdd
EthanPasman <epasman123@gmail.com>
2023-10-23 17:39:23 -0400

    Listening queue expanded rows WIP

--- script.js
+++ script.js
@@ -103,7 +103,6 @@ function clearForm() {
 }
 
 function addToLQueue(fmetadata) {
-    //TODO expand row for more metadata
     var audio = document.getElementById("audio");
     var table = document.getElementById("lqueueTable").getElementsByTagName("tbody")[0];
     var newRow = table.insertRow(table.rows.length);
@@ -112,8 +111,7 @@ function addToLQueue(fmetadata) {
     var cell3 = newRow.insertCell(2);
     var cell4 = newRow.insertCell(3);
 
-    cell1.innerHTML += fmetadata[1] + '<span class="artistLbl"> ' + fmetadata[2] + '</span>'; //Title Artist
-
+    cell1.innerHTML += fmetadata[1] + '<span class="artistLbl" title="Contributing artists: ' + fmetadata[4] + '"> ' + fmetadata[2] + '</span>'; //Title Artist (Contributors)
     cell2.innerHTML = fmetadata[3]; //Album
     if ("" + fmetadata[5] !== "NaN") /* Compare with string NaN */ {
         cell3.innerHTML = fmetadata[5]; //Year
@@ -129,6 +127,26 @@ function addToLQueue(fmetadata) {
         }
         cell4.innerHTML = m + ":" + s; //Length (m:ss)
     }
+
+    /*
+    //Expanded row for more metadata
+    var expNewRow = table.insertRow(table.rows.length);
+    var eCell1 = expNewRow.insertCell(0);
+    var eCell2 = expNewRow.insertCell(1);
+    var eCell3 = expNewRow.insertCell(2);
+
+    eCell1.innerHTML = fmetadata[7]; //Genres
+    eCell2.innerHTML = fmetadata[10]; //Tags
+    eCell3.colSpan = 2;
+    if ("" + fmetadata[6] !== "NaN") {
+        eCell3.innerHTML = fmetadata[6] + "BPM";
+    }
+
+    expNewRow.style.display = "none";
+    newRow.addEventListener("click", () => {
+        expNewRow.style.display == "none" ? expNewRow.style.display = "initial" : expNewRow.style.display = "none";
+    }); //Alternate visibility of expanded row on click
+    */
 }
 
 function hSettingsMenu() {
